36432352cfb3bf344b0cc706c3f07f4b9602d219
rluetken-dev <rluetken.dev@gmail.com>
2026-08-14 11:03:28 +0200

    feat(errors): add standardized problem details

--- src/IndustrialVisualAnomalyDetection.Api/Program.cs
+++ src/IndustrialVisualAnomalyDetection.Api/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = context =>
+    {
+        context.ProblemDetails.Extensions["traceId"] = context.HttpContext.TraceIdentifier;
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -13,6 +20,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseExceptionHandler();
+app.UseStatusCodePages();
 
 app.UseHttpsRedirection();
 
---
+++ tests/IndustrialVisualAnomalyDetection.Api.Tests/ProblemDetailsTests.cs
@@ -0,0 +1,36 @@
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace IndustrialVisualAnomalyDetection.Api.Tests;
+
+public sealed class ProblemDetailsTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public ProblemDetailsTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task UnknownEndpointReturnsProblemDetails()
+    {
+        using HttpClient client = _factory.CreateClient(
+            new WebApplicationFactoryClientOptions { BaseAddress = new Uri("https://localhost") });
+
+        using HttpResponseMessage response = await client.GetAsync("/endpoint-that-does-not-exist");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        Assert.Equal("application/problem+json", response.Content.Headers.ContentType?.MediaType);
+
+        ProblemDetails? problem = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+        Assert.NotNull(problem);
+        Assert.Equal(StatusCodes.Status404NotFound, problem.Status);
+        Assert.Equal("Not Found", problem.Title);
+        Assert.True(problem.Extensions.ContainsKey("traceId"));
+    }
+}
